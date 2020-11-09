7c000e0e29a57c2f0e6ad2963da696b3d6a5a253
Serugei-Kirririn <zombiliza616@gmail.com>
2020-11-09 22:08:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multiscreen/Brasillia.java
@@ -0,0 +1,15 @@
+package com.example.multiscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Brasillia extends Activity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.brasilia);
+    }
+}
---
+++ app/src/main/java/com/example/multiscreen/Columbia.java
@@ -0,0 +1,14 @@
+package com.example.multiscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Columbia extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.columbia);
+    }
+}
